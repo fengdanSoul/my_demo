15611b749c816091a4c380154ff8c48b536dd826
yj <yj@yjdeiMac.local>
2018-04-03 17:53:42 +0800

    stylus

---
+++ elem-dev/src/components/header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header">
+      <h1>I am header</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  .header
+    h1
+      color red
+
+
+</style>
